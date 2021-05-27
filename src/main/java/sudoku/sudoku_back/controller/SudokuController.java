b4f67294295c21975cbcf7ca8f9ecb5085510dc8
Chae Lin Shin <chaelinshin96@gmail.com>
2021-05-27 21:31:14 +0900

    Add another temporary code

--- src/main/java/sudoku/sudoku_back/controller/SudokuController.java
+++ src/main/java/sudoku/sudoku_back/controller/SudokuController.java
@@ -1,28 +1,26 @@
 package sudoku.sudoku_back.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sudoku.sudoku_back.model.SudokuModel;
+import sudoku.sudoku_back.service.SudokuService;
 
 import java.util.List;
-import java.util.Map;
 
-@RestController
+@Controller
 public class SudokuController {
-    @Autowired
-    private SudokuService sudokuService;
 
-//    SudokuController(
-//            SudokuService sudokuService
-//    ) {
-//        this.sudokuService = sudokuService;
-//    }
+    @Autowired
+    SudokuService sudokuService;
 
     @GetMapping("/sudoku")
-    @ResponseBody
-    public List<Map<String, Object>> getSudoku() {
-        return sudokuService.getSudoku();
+    public String list(Model model) {
+        List<SudokuModel> sudoku = sudokuService.printSudoku();
+        model.addAttribute("sudokuList", sudoku);
+        return "list";
     }
 
 }
