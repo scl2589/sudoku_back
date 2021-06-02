f4ff2cd0e87b2f15b277bfa769b8f8822362da08
Chae Lin Shin <chaelinshin96@gmail.com>
2021-06-02 10:59:55 +0900

    Add Swagger

--- src/main/java/sudoku/sudoku_back/controller/SudokuController.java
+++ src/main/java/sudoku/sudoku_back/controller/SudokuController.java
@@ -2,6 +2,9 @@ package sudoku.sudoku_back.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -48,6 +51,9 @@ public class SudokuController {
     }
 
     @GetMapping("/correct/{userSudoku}")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="userSudoku", value="사용자의 Sudoku 게임 플레이 결과", dataType="String", paramType="query", required=true, defaultValue="1 3 5 4 6 7 8 9 2 \\n6 8 7 1 2 9 4 3 5 \\n4 9 2 3 5 8 6 7 1 \\n2 1 4 5 3 6 7 8 9 \\n3 5 9 7 8 1 2 4 6 \\n8 7 6 2 9 4 1 5 3 \\n5 2 1 8 4 3 9 6 7 \\n7 6 8 9 1 5 3 2 4 \\n9 4 3 6 7 2 5 1 8 \\n")
+    })
     public Boolean checkCorrect(@PathVariable("userSudoku") String userSudoku) {
         Boolean isCorrect = sudokuService.checkCorrect(userSudoku);
         if (isCorrect == true) {
@@ -61,6 +67,9 @@ public class SudokuController {
 
     @Transactional
     @PostMapping("/sudoku")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="id", value="스도쿠 게임 기록 id", dataType="int", paramType="query", required=true)
+    })
     public void addSudoku(@RequestBody String sudokuData) throws JsonProcessingException, ParseException {
         ObjectMapper mapper = new ObjectMapper();
         JSONParser parser = new JSONParser();
@@ -72,6 +81,9 @@ public class SudokuController {
 
     @Transactional
     @DeleteMapping("/sudoku/{id}")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="id", value="스도쿠 기록 번호", dataType="int", paramType="query", defaultValue="1", required=true)
+    })
     public void deleteSudoku(@PathVariable("id") int id) {
         logger.info("스도쿠 게임 기록 " + id + "번이 삭제됩니다.");
         sudokuService.deleteSudoku(id);
